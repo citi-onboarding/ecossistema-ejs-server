b5463db34909af331c688c5d65163e9d890b758a
JoaoSouza9 <joaovictor.souza98@yahoo.com.br>
2021-12-09 20:57:09 -0300

    initial commit

---
+++ config/plugins.js
@@ -0,0 +1,28 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  }, 
+  email: {
+    provider: "nodemailer", 
+    providerOptions: { 
+      host: "smtp.gmail.com", 
+      port: 587, 
+      auth: {
+        user: env('EMAIL_SMTP_USER'), 
+        pass: env('EMAIL_SMTP_PASS'),
+      },
+		settings: {
+				defaultFrom: "Nome desejado" 
+      },
+    },
+  },
+});
